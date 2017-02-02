44babdf934eb3edb75bb0f9ba65b76fb2c74002e
Scott123180 <Scott123180@gmail.com>
2017-02-02 10:28:30 -0500

    working on input

--- lexer.cpp
+++ lexer.cpp
@@ -8,14 +8,22 @@ token genToken(string type, string data, int line);
 
 int main()
 {
-  char eof;
-  cin >> eof;
+  bool eof = false; //end of file
 
-  while(eof != '$')
+  while(!eof) //loop until end of file
   {
-    char curchar;
-    cin >> curchar;
-    
+    char curLine;
+    getline(cin, curLine); 
+   
+     
+    //loop through string
+    for(int i = 0; i < curLine.length(); i++)
+    {
+      //create buffer to recognize regex patterns
+      
+
+      //craft token from buffer
+    } 
   }
   return 0;
 }
