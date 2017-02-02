7a3b81fe110271e6b853ea8ffd297c4c35126665
Scott123180 <Scott123180@gmail.com>
2017-02-02 10:11:05 -0500

    starting lexer

---
+++ lexer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+token genToken(string type, string data, int line);
+
+int main()
+{
+  char eof;
+  cin >> eof;
+
+  while(eof != '$')
+  {
+    char curchar;
+    cin >> curchar;
+    
+  }
+  return 0;
+}
