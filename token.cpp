7cadf6a1c047ce4a41a35ecf37ff2e223aa2e80d
Scott123180 <Scott123180@gmail.com>
2017-02-07 16:04:02 -0500

    token matching

--- token.cpp
+++ token.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <map>
+
 #include "disease.h"
 
 using namespace std;
 
 class Token
 {
-  Token::Token(string t, string d, int l, int p) : type(t), data(d), line(l), position(p)
+  //type known
+  Token::Token(string t, string d, int l, int p) : type(t), data(d), line(l), position(p) {
+  if(t == "leftQuote") {}
+  else if(t == "rightQuote") {}
+  else if(t == "charList") {}
+ 
+
+  }
+  //type unknown
+  Token::Token(string d, int l, int p) : data(d), line(l), position(p)
+  {
+    //map str to str
+    map textToType<string, string> = {
+      {"print", "print"
+    //based on type, do conversions / checking 
+
+    //id's | if length one and is a-z, then set type as id
+  
+    //call function to look through map without adding to it if it doesn't find anything
+  }
+
+  Token::error(string errorType, int line, int pos)
   {
 
   }
--- token.h
+++ token.h
@@ -3,18 +3,36 @@
 
 #include <string>
 #include <vector>
+#include <map>
 
 class Token
 {
   public:
-    Token::genToken(string t, string d, int l, int p);
-    Token::genToken(string d,int l, int p);
+    Token(string t, string d, int l, int p);
+    Token(string d,int l, int p);
   private:
     string type;
     string data;
     int line;
     int position;
     
+    map textToType<string, string> = {
+      {"print", "typePrint"},
+      {"int", "typeInt"},
+      {"string", "typeString"}, 
+      {"boolean", "typeBoolean"},
+      {"=", "assignOp"},
+      {"==", "boolOp"},
+      {"!=", "boolOp"},
+      {"true", "boolVal"},
+      {"false", "boolVal"},
+      {"while", "whileStatement"},
+      {"if", "ifStatement"},
+      {"{", "leftBrace"},
+      {"}", "rightBrace"},
+      {"(", "leftParen"},
+      {")", "rightParen"}
 };
+
 
 #endif
