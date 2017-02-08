c4824fc5516616be86d7b838d7fcb15399fa05ef
Scott123180 <Scott123180@gmail.com>
2017-02-07 23:21:32 -0500

    working

---
+++ project/not_main.cpp
@@ -0,0 +1,16 @@
+/*
+
+#include <iostream>
+
+#include "lexer.h"
+#include "token.h"
+
+using namespace std;
+
+int main()
+{
+  //lexer commands 
+  
+  return 0;
+}
+*/
--- project/token.h
+++ project/token.h
@@ -14,6 +14,7 @@ class Token
   public:
     Token(string t, string d, int l, int p);
     Token(string d,int l, int p);
+
     string getType();
     string getData();
     int getLine();
