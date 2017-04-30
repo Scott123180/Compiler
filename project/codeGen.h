37a7a0c99c6b1e543e0a8c46a28f1536200b438b
Scott123180 <Scott123180@gmail.com>
2017-04-30 10:21:08 -0400

    op codes

--- project/codeGen.h
+++ project/codeGen.h
@@ -24,28 +24,29 @@ class CodeGen
     //Add with carry
     const string ADC = "6D";
     //load the X register with a constant
-
+    const string LDX_C = "A2";
     //load the X register from memory
-
+    const string LDX_M = "AE";
     //load the Y register with a constant
-
+    const string LDY_C = "A0";
     //load the Y register from memory
-
+    const string LDY_M = "AC";
     //no operation
-
+    const string NOP = "EA";
     //break (which is really a system call)
-
+    const string BRK = "00";
     //compare a byte in memory to the X reg - Sets the Z(zero) flag if equal
-
+    const string CPX = "EC";
     //branch n bytes if Z flag = 0
-
+    const string BNE = "D0";
     //increment the value of a byte
-
+    const string INC = "EE";
     //System calls
-
+    const string SYS = "FF";
       //01 in x reg = print the integer stored in the y register
-
-      //03 in x reg = print the 00-terminated string stored at the address in the y register
+      const string P_INT = "01";
+      //02 in x reg = print the 00-terminated string stored at the address in the y register
+      const string P_STR = "02";
 };
 
 #endif //COMPILER_CODEGEN_H
